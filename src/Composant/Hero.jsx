8ee89a1db0304ad02e28395a5fc6a59142618555
Oumar Diane <oumardiane399@gmail.com>
2025-09-02 18:14:09 +0200

    plus

--- src/Composant/Hero.jsx
+++ src/Composant/Hero.jsx
@@ -4,7 +4,7 @@ import '../Style/Hero.css';
 // Import des images
 import bg1 from '../assets/Image/P1035980.webp';
 import bg2 from '../assets/Image/pont.webp';
-import bg3 from '../assets/Image/P1035800.webp';
+import bg3 from '../assets/Image/projet.webp';
 
 const HeroSection = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
